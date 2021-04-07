94a216a388e568d5fb7d619274b23ebc4d0fc46b
spr0ut <30391853+spr0ut@users.noreply.github.com>
2021-04-07 13:54:38 +0200

    logging

--- libs/smbserver.py
+++ libs/smbserver.py
@@ -3506,6 +3506,7 @@ class SMBSERVERHandler(SocketServer.BaseRequestHandler):
         SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
 
     def handle(self):
+        LOG.info("Incoming connection...")
         self.__SMB.log("Incoming connection (%s,%d)" % (self.__ip, self.__port))
         self.__SMB.addConnection(self.__connId, self.__ip, self.__port)
         while True:

d538b44890bc48c6b13d9defb43abc553c3545bb
spr0ut <eilsaadi@protonmail.com>
2021-04-07 13:03:54 +0200

    remove tcpdump from startup

--- startup_scripts.sh
+++ startup_scripts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python /home/smb/smbserver.py &  
-tcpdump -p -f "port 445 or port 139" -w /home/smb/logs/smblog.pcap
+python /home/smb/smbserver.py #&  
+# tcpdump -p -f "port 445 or port 139" -w /var/log/smblog.pcap
